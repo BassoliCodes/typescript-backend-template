dea79ab56690218f9b5585c7159c7e99aaf60ab2
Lucca Bassoli <lucca.bassoli2020@gmail.com>
2022-02-14 08:52:23 -0300

    update

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,21 @@
+import { User } from '@prisma/client';
+import prismaClient from '../../../../prisma';
+import { ICreateUsersDTO } from '../../dtos/ICreateUsersDTO';
+import { IUsersRepository } from '../IUsersRepository';
+
+class UsersRepository implements IUsersRepository {
+  async create({ name, email, password }: ICreateUsersDTO): Promise<User> {
+    const user = await prismaClient.user.create({
+      data: {
+        name,
+        email,
+        password,
+        isAdmin: false,
+      },
+    });
+
+    return user;
+  }
+}
+
+export { UsersRepository };
